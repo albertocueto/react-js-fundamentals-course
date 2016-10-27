0a36909d07c43a2b71ac1fa13c12bf6785f53565
Alberto Cueto <cueto.alberto@gmail.com>
2016-10-27 00:16:27 -0500

    dding example source code of how to use the content of custom components

---
+++ LESSON-7:React-with-es6-syntax/use-content-of-custom-components/app/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {render} from 'react-dom'
+import ReactDOM from 'react-dom'
+
+class AnotherComponent extends React.Component {
+  render() {
+    return(
+      <p>This is <i>another</i> component</p>
+    )
+  }
+}
+
+class MyComponent extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>My Component</h1>
+        {this.props.children}
+        <p>More content</p>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+        <MyComponent>
+          Arbitrary content
+          <AnotherComponent />
+        </MyComponent>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
